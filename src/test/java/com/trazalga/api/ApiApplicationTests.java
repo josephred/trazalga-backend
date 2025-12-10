30e8a6efa125037bee56ee91f0aad727c21d7570
Aar Garate <agarate@outlook.cl>
2025-12-10 21:39:28 +0000

    add

--- src/test/java/com/trazalga/api/ApiApplicationTests.java
+++ src/test/java/com/trazalga/api/ApiApplicationTests.java
@@ -2,8 +2,14 @@ package com.trazalga.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.jpa.defer-datasource-initialization=true"
+})
 class ApiApplicationTests {
 
 	@Test
