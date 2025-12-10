4d0b4e0ad5db269b479e668cde9587742d38b2bb
Aar <agarate@outlook.cl>
2025-12-10 01:14:05 -0300

    Configure test datasource initialization and lazy load settings

--- src/test/java/com/trazalga/api/ApiApplicationTests.java
+++ src/test/java/com/trazalga/api/ApiApplicationTests.java
@@ -3,9 +3,13 @@ package com.trazalga.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.jpa.defer-datasource-initialization=true"
+})
 class ApiApplicationTests {
 
 	@Test
