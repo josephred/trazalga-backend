3cfcc957549f3fe575b1864936a9474363f1b5ef
Aaron Garate <agarate@outlook.cl>
2026-09-09 16:20:44 -0300

    fix(macrozona): remove @CrossOrigin to prevent validateAllowCredentials conflict with global CorsConfig

--- src/main/java/com/trazalga/api/controllers/MacrozonaController.java
+++ src/main/java/com/trazalga/api/controllers/MacrozonaController.java
@@ -7,7 +7,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +25,6 @@ import com.trazalga.api.services.RegionService;
 
 @RestController
 @RequestMapping("/api/macrozonas")
-@CrossOrigin(origins = "*")
 public class MacrozonaController {
 
     @Autowired
